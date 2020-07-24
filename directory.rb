ba914135a8c64e9da748d4ea6206076e466e7b37
Zish Jawaid <zisheanj@gmail.com>
2020-07-24 16:52:21 +0100

    Refactor: asking for user input

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# Let's puts all students in to an array
+=begin
 students = [
   {:name => "Dr. Hannibal Lecter", :cohort => :august},
   {:name => "Darth Vader", :cohort => :august},
@@ -12,6 +12,25 @@ students = [
   {:name => "Joffrey Baratheon", :cohort => :august},  
   {:name => "Norman Bates", :cohort => :august}
 ]
+=end
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -21,14 +40,15 @@ end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end  
+  end
 end
-
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
