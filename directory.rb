774374ee43e47f716e06752d79e66e2601e96f6c
Zish Jawaid <zisheanj@gmail.com>
2020-07-27 14:39:51 +0100

    Refactor: abstract code in to shorter methods

--- directory.rb
+++ directory.rb
@@ -13,70 +13,75 @@ students = [
   {:name => "Norman Bates", :cohort => :august}
 ]
 
+ # Declare instance variable so all methods can access it. Set variable equal to an empty array.
+ # Make sure it is set outside the loop so it doesn't get reset after the first iteration.
 
-def interactive_menu
-  # set variable equal to an empty array. Set outside the loop
-  # so it exists across many iterations, not just the first iteration
-  students = []
-  # repeat until the user exits
-  loop do
-  # print the menu of options and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    # read user input and assign to a variable
-    selection = gets.chomp
-
-    # do what the user has asked
-    case selection
-
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      else
-        puts "I don't know what you mean, try again"
-    end
-  end  
-end
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+end 
+
+def interactive_menu 
+  # repeat until the user exits
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
 end
 
+# do what the user has asked   
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students 
+    when "9"     
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you mean, try again"
+  end  
+end
+
+def print_menu
+  #print the menu of options and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+ 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-
 
 #nothing happens until we call the methods
 interactive_menu
