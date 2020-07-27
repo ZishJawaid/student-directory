5e18d409b90e9fb8913db6b84a7510544e9b0f09
Zish Jawaid <zisheanj@gmail.com>
2020-07-27 19:08:55 +0100

    Refactor: Add functionality to load students from external file

--- directory.rb
+++ directory.rb
@@ -50,6 +50,8 @@ def process(selection)
       show_students 
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"     
       exit # this will cause the program to terminate
     else
@@ -62,6 +64,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -91,17 +94,20 @@ def save_students
   file = File.open("students.csv", "w")
   # Iterate over the array of students
   @students.each do |student|
-
     student_data = [student[:name], student[:cohort]]
-
     csv_line = student_data.join(",")
-
     file.puts csv_line
-
   end
-
   file.close  
+end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << { :name => name, :cohort => cohort.to_sym }
+  end
+file.close
 end
 
 #nothing happens until we call the methods
